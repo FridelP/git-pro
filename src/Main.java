e5e0940edffe713fc132b96a25b84900dbae7ff4
Mate Matok <matokmate42@gmail.com>
2023-12-04 14:11:51 +0100

    c5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         count("aprilis");
-        cheer("Java");
+        cheer("Java","assholes");
         greet("Greetings");
     }
 
@@ -13,7 +13,7 @@ public class Main {
         System.out.println(name);
     }
 
-    public static void cheer(String cheer) {
-        System.out.println(cheer);
+    public static void cheer(String cheer,String param) {
+        System.out.println(cheer+param);
     }
 }
