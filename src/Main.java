ff9fb25af62d95f5fa046f73bff6099e731ca3df
Fenyő Kristóf <fenyo.kr@gmail.com>
2023-12-04 14:37:39 +0100

    redo

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         count("aprilis", "update");
-        cheer("Java","assholes");
-        greet("Greetings","István");
+        cheer("Java");
+        greet("Greetings");
     }
 
     public static void count(String count, String count2) {
         System.out.println(count);
     }
 
-    public static void greet(String name,String lastName) {
-        System.out.println(name+lastName);
+    public static void greet(String name) {
+        System.out.println(name);
     }
 
-    public static void cheer(String cheer,String param) {
-        System.out.println(cheer+param);
+    public static void cheer(String cheer) {
+        System.out.println(cheer);
     }
 }
