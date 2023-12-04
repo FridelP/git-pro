833fd8b6d49a375c28e4840b86c2e9a2b15da1cd
Mate Matok <matokmate42@gmail.com>
2023-12-04 14:39:30 +0100

    redoagain

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         count("aprilis");
         cheer("Java","assholes");
-        greet("Greetings","István");
+        greet("Greetings");
     }
 
     public static void count(String count) {
         System.out.println(count);
     }
 
-    public static void greet(String name,String lastName) {
-        System.out.println(name+lastName);
+    public static void greet(String name) {
+        System.out.println(name);
     }
 
     public static void cheer(String cheer,String param) {
