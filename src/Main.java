18e3a978848d33a0e0012fb48ac45634527fa692
Fridel Istvan <istvan.fridel@gmail.com>
2023-12-04 14:08:33 +0100

    a5

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         count("aprilis");
         cheer("Java");
-        greet("Greetings");
+        greet("Greetings","István");
     }
 
     public static void count(String count) {
         System.out.println(count);
     }
 
-    public static void greet(String name) {
-        System.out.println(name);
+    public static void greet(String name,String lastName) {
+        System.out.println(name+lastName);
     }
 
     public static void cheer(String cheer) {
